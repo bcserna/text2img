001a877dd7b79cd6ac3dabd4c7991da2ac3e6405
bcserna <cserna.balazs@gmail.com>
2019-05-06 00:00:16 +0200

    Major shape fixes

--- src/generator.py
+++ src/generator.py
@@ -2,26 +2,27 @@ import torch
 from torch import nn
 
 from src.Attention import Attention
-from src.util import upsample, residual_block
+from src.util import upsample, residual_block, conv_3x3
 
-from src.config import D_Z, D_COND, D_GF, RESIDUALS, D_HIDDEN
+from src.config import D_Z, D_COND, D_GF, RESIDUALS, D_HIDDEN, D_WORD
 
 
 class Generator0(nn.Module):
     def __init__(self):
         super().__init__()
+        self.d_gf = D_GF * 16
         self.fc = nn.Sequential(
-            nn.Linear(D_Z + D_COND, D_GF * 4 * 4 * 2, bias=False),
-            nn.BatchNorm1d(D_GF * 4 * 4 * 2),
+            nn.Linear(D_Z + D_COND, self.d_gf * 4 * 4 * 2, bias=False),
+            nn.BatchNorm1d(self.d_gf * 4 * 4 * 2),
             nn.modules.activation.GLU()
         )
 
-        self.upsample_steps = nn.Sequential(*[upsample(D_GF // (2 ** i), D_GF // (2 ** (i + 1))) for i in range(4)])
+        self.upsample_steps = nn.Sequential(*[upsample(self.d_gf // (2 ** i), self.d_gf // (2 ** (i + 1))) for i in range(4)])
 
     def forward(self, z_code, c_code):
         x = torch.cat((c_code, z_code), 1)
         x = self.fc(x)
-        x = x.view(-1, D_GF, 4, 4)  # -> D_GF x 4 x 4
+        x = x.view(-1, self.d_gf, 4, 4)  # -> D_GF x 4 x 4
         x = self.upsample_steps(x)
 
         return x  # D_GF/16 x 64 x 64
@@ -31,7 +32,7 @@ class GeneratorN(nn.Module):
     def __init__(self):
         super().__init__()
         self.residuals = nn.Sequential(*[residual_block(D_GF * 2) for _ in range(RESIDUALS)])
-        self.attn = Attention(D_GF, D_HIDDEN)
+        self.attn = Attention(D_GF, D_WORD)
         self.upsample = upsample(D_GF * 2, D_GF)
 
     def forward(self, h_code, c_code, word_embs, mask):
@@ -47,3 +48,15 @@ class GeneratorN(nn.Module):
         out_code = self.residuals(h_c_code)
         out_code = self.upsample(out_code)  # D_GF/2 x 2in_size x 2in_size
         return out_code, att
+
+
+class ImageGen(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.img = nn.Sequential(
+            conv_3x3(D_GF, 3),
+            nn.Tanh()
+        )
+
+    def forward(self, h_code):
+        return self.img(h_code)
