84d1196b8fd52fee2cd53d25a901177de5e5eee7
bcserna <cserna.balazs@gmail.com>
2019-07-31 22:58:33 +0200

    Refactor data subsets, fix caption pairing

--- src/data.py
+++ src/data.py
@@ -15,14 +15,22 @@ from src.config import BASE_SIZE, BRANCH_NUM, CAPTIONS, END_TOKEN, CAP_LEN, BATC
 
 
 class CUBSubset(Dataset):
-    def __init__(self, subset, captions, img_size, transforms, normalize):
+    def __init__(self, subset, img_size, transformations, normalize, preload):
         self.data = subset
-        self.captions = captions
         self.img_size = img_size
-        self.transforms = transforms
+        self.transforms = transformations
         self.normalize = normalize
+        self.images = None
+        if preload:
+            self.load_images()
+
+    def load_images(self):
+        self.images = [self.get_image(i) for i in tqdm(range(1, len(self)), desc='Preloading images')]
 
     def __getitem__(self, index):
+        if self.images is not None:
+            return self.images[index + 1]
+
         index = index + 1  # Image index starts from 1
         imgs = self.get_image(index)
         caption = self.get_caption(index)
@@ -88,7 +96,7 @@ class CUBSubset(Dataset):
         return ret
 
 
-class CUB(Dataset):
+class CUB(object):
     def __init__(self, min_word_freq=MIN_WORD_FREQ):
         print('Loading image paths ...')
         self.data = pd.read_csv('CUB_200_2011/images.txt', delim_whitespace=True, header=None, index_col=0,
@@ -96,7 +104,10 @@ class CUB(Dataset):
 
         print('Loading image captions, counting word frequencies, building vocab ...')
         self.word_freq = {}
-        self.captions = {}
+
+        for i in range(CAPTIONS):
+            self.data[f'caption_{i}'] = ''
+
         for i, row in self.data.iterrows():
             dir, file = row['img_path'].split('/')
             file = file.replace('.jpg', '.txt')
@@ -106,9 +117,9 @@ class CUB(Dataset):
                 lines = f.readlines()
                 # Keep only alphanumeric characters
                 captions = [pattern.sub('', line.lower().strip()) for line in lines]
-                self.captions[i] = captions
 
-                for c in captions:
+                for j, c in enumerate(captions):
+                    row[f'caption_{j}'] = c
                     for word in c.split():
                         self.word_freq[word] = self.word_freq.get(word, 0) + 1
 
@@ -152,78 +163,8 @@ class CUB(Dataset):
                                    index_col=0, names=['class'])
         self.data = self.data.join(class_labels)
 
-        # print('Loading images...')
-        self.images = []
-        # self.load_images()
+        self.train = CUBSubset(self.data[self.data.train == 1], self.imsize, self.transforms, self.normalize,
+                               preload=False)
+        self.test = CUBSubset(self.data[self.data.train == 0], self.imsize, None, self.normalize, preload=True)
+
         print('Done.')
-
-    def get_caption(self, index):
-        caption_idx = random.randint(0, CAPTIONS - 1)
-        caption = self.captions[index][caption_idx]
-        encoded = [self.vocab[w] for w in caption.split()]
-        cap_len = len(encoded)
-        encoded = encoded[:CAP_LEN - 1]  # address END token
-        encoded = encoded + self.vocab[END_TOKEN] * (CAP_LEN - cap_len)
-        return encoded
-
-    def get_image(self, index):
-        if self.images is not None:
-            return self.images[index]
-
-        img_data = self.data.loc[index, :]
-        img = Image.open('CUB_200_2011/images/' + img_data.img_path).convert('RGB')
-        width, height = img.size
-
-        r = int(np.maximum(img_data.bbox_width, img_data.bbox_height) * 0.75)
-        center_x = img_data.bbox_x + img_data.bbox_width // 2
-        center_y = img_data.bbox_y + img_data.bbox_height // 2
-        y1 = np.maximum(0, center_y - r)
-        y2 = np.minimum(height, center_y + r)
-        x1 = np.maximum(0, center_x - r)
-        x2 = np.minimum(width, center_x + r)
-        img = img.crop([x1, y1, x2, y2])
-        img = self.transforms(img)
-
-        imgs = []
-        for i in range(BRANCH_NUM):
-            if i < (BRANCH_NUM - 1):
-                re_img = transforms.Resize(self.imsize[i])(img)
-            else:
-                re_img = img
-            imgs.append(self.normalize(re_img))
-
-        return imgs
-
-    def load_images(self):
-        self.images = [self.get_image(i) for i in tqdm(range(1, len(self)))]
-
-    def __getitem__(self, index):
-        index = index + 1  # Image index starts from 1
-        imgs = self.get_image(index)
-        caption = self.get_caption(index)
-        label = self.data['class'][index]
-        return imgs, caption, label
-
-    def __len__(self):
-        return self.data.train.sum()
-
-    @staticmethod
-    def collate_fn(batch):
-        ret = {
-            'img64': [],
-            'img128': [],
-            'img256': [],
-            'caption': [],
-            'label': []
-        }
-        for img, cap, label in batch:
-            ret['img64'].append(img[0])
-            ret['img128'].append(img[1])
-            ret['img256'].append(img[2])
-            ret['caption'].append(cap)
-            ret['label'].append(label)
-
-        ret['img64'] = torch.stack(ret['img64'])
-        ret['img128'] = torch.stack(ret['img128'])
-        ret['img256'] = torch.stack(ret['img256'])
-        return ret
