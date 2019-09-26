28d7c3b35677c51f18c1a12e38bf1f41b0d0c87d
Balazs Cserna <cserna.balazs@gmail.com>
2019-09-26 19:03:33 +0200

    Fix setting image encoder params to device

--- src/encoder.py
+++ src/encoder.py
@@ -49,13 +49,13 @@ class ImageEncoder(nn.Module):
     def __init__(self, device=DEVICE):
         super().__init__()
         self.device = device
-        self.inception_model = torchvision.models.inception_v3(pretrained=True)
+        self.inception_model = torchvision.models.inception_v3(pretrained=True).to(self.device)
         # Freeze Inception V3 parameters
-        self.inception_model.requires_grad_(False)
+        self.inception_model.requires_grad_(False).to(self.device)
         # 768: the dimension of mixed_6e layer's sub-regions (768 x 289 [number of sub-regions, 17 x 17])
-        self.local_proj = conv1x1(768, D_HIDDEN)
+        self.local_proj = conv1x1(768, D_HIDDEN).to(self.device)
         # 2048: the dimension of last average pool's output
-        self.global_proj = nn.Linear(2048, D_HIDDEN)
+        self.global_proj = nn.Linear(2048, D_HIDDEN).to(self.device)
 
         self.local_proj.weight.data.uniform_(-IMG_WEIGHT_INIT_RANGE, IMG_WEIGHT_INIT_RANGE)
         self.global_proj.weight.data.uniform_(-IMG_WEIGHT_INIT_RANGE, IMG_WEIGHT_INIT_RANGE)
