0d0f4c2bfbff9d2d9ef8ff8f62ac3a4976ea08ea
bcserna <cserna.balazs@gmail.com>
2019-10-01 09:15:37 +0200

    Improve usability

--- src/train.py
+++ src/train.py
@@ -6,12 +6,13 @@ from src.generator import Generator
 from src.discriminator import Discriminator, PatchDiscriminator
 
 
-def train_gan(epochs, device=DEVICE):
+def train_gan(epochs, gan=None, device=DEVICE):
     cub = CUB()
     damsm = DAMSM.load('l01992')
-    generator = Generator(device)
-    # discriminator = Discriminator(device)
-    discriminator = PatchDiscriminator(device)
-    gan = AttnGAN(damsm, generator, discriminator, device)
+    if gan is None:
+        generator = Generator(device)
+        # discriminator = Discriminator(device)
+        discriminator = PatchDiscriminator(device)
+        gan = AttnGAN(damsm, generator, discriminator, device)
     metrics = gan.train(cub, epochs)
-    return metrics
+    return gan, metrics
