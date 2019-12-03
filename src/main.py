6b358029d4f81882b6b6cf81c116a8f804b4ffb5
bcserna <cserna.balazs@gmail.com>
2019-12-03 14:19:34 +0100

    Rename module, add validation script

--- src/main.py
+++ src/main.py
@@ -30,6 +30,18 @@ def train_gan(epochs, name, gan, damsm, device):
 
 
 @main.command()
+@click.argument('gan')
+@click.argument('damsm')
+@click.argument('save_dir')
+@click.option('--device', default=DEVICE)
+def validate_gan(gan, damsm, save_dir, device):
+    dataset = CUB()
+    damsm_model = DAMSM.load(damsm, device=device)
+    gan_model = AttnGAN.load(gan, damsm_model, device=device)
+    gan_model.validate_test_set(dataset, save_dir=save_dir)
+
+
+@main.command()
 @click.argument('epochs', type=int)
 @click.argument('name')
 @click.option('--patience', type=int, default=20)
